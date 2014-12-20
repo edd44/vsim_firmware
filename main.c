4427bc67bb43c902df2c63c9c9eab8e763a3aa15
Michal Kaptur <kaptur@webmail.nsn-intra.net>
2014-12-20 19:10:18 +0100

    added usart prints

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <avr/io.h>
 #include <util/delay.h>
+#include "mklib_usart/mklib_usart.h"
 
 #define T_ON OCR1A
 
@@ -7,19 +8,22 @@ const int cycle_time_us = 20000;
 
 int main()
 {
-		DDRD |= 0xFF;
-		TCCR1A |= 1<<WGM11 | 1<<COM1A1 ;
-		TCCR1B |= 1<<WGM13 | 1<<WGM12 | 1<<CS11;
-		ICR1 = cycle_time_us;
+	USART_stdout_redirect();
+	USART_init(9600, 0, 1, 0, 0);
+	printf("dzindybry!");
+	DDRD |= 0xFF;
+	TCCR1A |= 1<<WGM11 | 1<<COM1A1 ;
+	TCCR1B |= 1<<WGM13 | 1<<WGM12 | 1<<CS11;
+	ICR1 = cycle_time_us;
 
-		int i = 2000;
-		while (1)
-			{
-				T_ON = i;
-				i>=3400 ? i=0 : i++;
-				_delay_ms(1);
-			}
-
+	int i = 2000;
+	while (1)
+		{
+			T_ON = i;
+			printf("%d\n",i);
+			i>=2300 ? i=500 : i++;
+			//_delay_ms(1);
+		}
 
 	return 0;
 }
