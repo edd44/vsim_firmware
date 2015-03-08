4057ff3ef64d1b8461bdcd4eebac8b98b18322a1
edd44 <kaptur.michal@gmail.com>
2015-03-08 23:01:11 +0100

    add: 2nd servo control

--- main.c
+++ main.c
@@ -3,10 +3,37 @@
 #include "mklib_usart/mklib_usart.h"
 #include "receiver.h"
 
-#define PWM_T_ON_US OCR1A
+#define PWM_T_ON_US_0 OCR1A
+#define PWM_T_ON_US_1 OCR1B
 #define PWM_PERIOD_US ICR1
 
 const int cycle_time_us = 20000;
+
+void set_servo(uint8_t servo_nr, uint8_t value)
+{
+	switch (servo_nr)
+	{
+		case 0:
+			PWM_T_ON_US_0 = value *10;
+			break;
+		case 1:
+			PWM_T_ON_US_1 = value *10;
+			break;
+		default:
+			break;
+	}
+}
+
+void config_timer()
+{
+	//FAST PWM, /8 prescaler, clear OC1A on compare match
+	DDRD |= 0xFF;
+	TCCR1A |= 1<<COM1A1 | 1<<COM1B1; //clear output on compare match
+	TCCR1A |= 1<<WGM11 ;
+	TCCR1B |= 1<<WGM13 | 1<<WGM12 | 1<<CS11;
+	PWM_PERIOD_US = cycle_time_us;
+
+}
 
 int main()
 {
@@ -14,12 +41,10 @@ int main()
 	USART_init(19200, RX_ENABLE, TX_ENABLE, RX_INT_DISABLE, TX_INT_DISABLE);
 	printf("\nHello there my little fellow!\n");
 
-	DDRD |= 0xFF;
-	TCCR1A |= 1<<WGM11 | 1<<COM1A1 ;
-	TCCR1B |= 1<<WGM13 | 1<<WGM12 | 1<<CS11;
-	PWM_PERIOD_US = cycle_time_us;
+	config_timer();
+	set_servo(0, 150);
+	set_servo(1, 50);
 
-	uint8_t i = 70;
 	while (1)
 		{
 			struct actors_settings_t settings = receive(USART_getchar());
@@ -29,8 +54,8 @@ int main()
 					{
 						printf("Servo #%d: %d\n", i, settings.value[i]);
 					}
-				//TO DO: create an interface for setting servos value
-				PWM_T_ON_US = settings.value[0]*10;
+				set_servo(1, settings.value[1]);
+				set_servo(0, settings.value[0]);
 			}
 		}
 
